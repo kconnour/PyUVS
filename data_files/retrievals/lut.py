eca9c152d03cc8165807aa56e76bd217b1d5ea8e
Kyle <kyle.d.connour@gmail.com>
2023-08-07 17:32:09 -0600

    Add LUT creation routine and retrieval update

---
+++ data_files/retrievals/lut.py
@@ -0,0 +1,278 @@
+from pathlib import Path
+import time
+import os
+from tempfile import mkdtemp
+import multiprocessing as mp
+
+import numpy as np
+from astropy.io import fits
+import pyrt
+import disort
+
+iuvs_wavelengths = np.array([0.210, 0.215, 0.220, 0.272, 0.278, 0.284])
+z = np.linspace(100, 0, num=15)
+altitude_midpoint = (z[:-1] + z[1:]) / 2
+temperature = np.ones(15,) * 175
+
+################
+## Define Radprop
+################
+
+class Radprop:
+    def __init__(self):
+        self._base_path = Path('/media/kyle/iuvs/radiative_properties/wolff')
+        self.hdul = None
+
+    def _get_file_forward_scattering_properties(self) -> np.ndarray:
+        return self.hdul['forw'].data
+
+    def _get_file_legendre_coefficients(self) -> np.ndarray:
+        return self.hdul['pmom'].data
+
+    def _get_file_phase_function(self) -> np.ndarray:
+        return self.hdul['phsfn'].data
+
+    def _get_file_phase_function_reexpansion(self) -> np.ndarray:
+        return self.hdul['expansion'].data
+
+    def get_primary(self) -> np.ndarray:
+        """Get the primary hdul.
+
+        Returns
+        -------
+        The primary structure
+
+        Notes
+        -----
+        This array should contain no relevant info.
+
+        """
+        return self.hdul['primary'].data
+
+    def get_particle_sizes(self) -> np.ndarray:
+        """Get the particle sizes associated with each of the radiative properties.
+
+        Returns
+        -------
+        The particle sizes
+
+        Notes
+        -----
+        The particle sizes are the centers of some particle size distribution.
+
+        """
+        return self.hdul['particle_sizes'].data
+
+    def get_wavelengths(self) -> np.ndarray:
+        return self.hdul['wavelengths'].data
+
+    def get_scattering_angles(self) -> np.ndarray:
+        return self.hdul['scattering_angle'].data
+
+    def get_scattering_cross_sections(self) -> np.ndarray:
+        return self._get_file_forward_scattering_properties()[..., 1]
+
+    def get_extinction_cross_sections(self) -> np.ndarray:
+        return self._get_file_forward_scattering_properties()[..., 0]
+
+    def get_asymmetry_parameters(self) -> np.ndarray:
+        return self._get_file_forward_scattering_properties()[..., 2]
+
+    def get_phase_functions(self) -> np.ndarray:
+        return np.moveaxis(self._get_file_phase_function(), 0, -1)
+
+    def get_legendre_coefficients(self) -> np.ndarray:
+        return self._get_file_legendre_coefficients()  # This is stupid but it needs a quick fix
+
+    def get_phase_function_reexpansions(self) -> np.ndarray:
+        return np.moveaxis(self._get_file_phase_function_reexpansion(), 0, -1)
+
+    def _get_header(self) -> fits.header.Header:
+        return self.hdul['primary'].header
+
+    def get_file_creation_date(self) -> str:
+        header = self._get_header()
+        return header['date']
+
+    def get_history(self) -> str:
+        header = self._get_header()
+        history = str(header['history'])
+        # The ??? currently only applies to the dust file
+        return history.replace('???', '--')
+
+
+class Dust(Radprop):
+    def __init__(self):
+        super().__init__()
+        self.hdul = fits.open(self._base_path / 'dust01-mars045i_all_area_s0780.fits.gz')
+
+
+class Ice(Radprop):
+    def __init__(self):
+        super().__init__()
+        self.hdul = fits.open(self._base_path / 'ice01-droxtal_050_tmat1_reff_v010.fits.gz')
+
+
+# Read in radprop
+# I have no f****** clue why I can't call these methods later on. If I define variables here, the multiprocessing
+# works. If not, it breaks when these are called in parallel. It works when called in series
+dust_radprop = Dust()
+dust_particle_sizes = dust_radprop.get_particle_sizes()
+dust_wavelengths = dust_radprop.get_wavelengths()
+dust_extinction_cross_sections = dust_radprop.get_extinction_cross_sections()
+dust_scattering_cross_sections = dust_radprop.get_scattering_cross_sections()
+dust_legendre_coefficients = dust_radprop.get_legendre_coefficients()
+
+ice_radprop = Ice()
+ice_particle_sizes = ice_radprop.get_particle_sizes()
+ice_wavelengths = ice_radprop.get_wavelengths()
+ice_extinction_cross_sections = ice_radprop.get_extinction_cross_sections()
+ice_scattering_cross_sections = ice_radprop.get_scattering_cross_sections()
+ice_legendre_coefficients = ice_radprop.get_legendre_coefficients()
+
+n_streams = 8
+n_polar = 1  # defined by IUVS' viewing geometry
+n_azimuth = 1  # defined by IUVS' viewing geometry
+
+###############
+### Surface
+###############
+# Get Mike's Hapke w at the IUVS wavelengths for the LUT
+wolff_hapke = np.interp(iuvs_wavelengths, np.linspace(0.258, 0.32, num=100), np.linspace(0.07, 0.095, num=100))
+# I can't do any more here because the surface is a function of SZA, EA, and PA (or azimuth angle)
+clancy_lambert_albedo = np.interp(iuvs_wavelengths, np.linspace(0.2, 0.33, num=100), np.linspace(0.01, 0.015, num=100))
+
+##############
+# Output arrays
+##############
+n_user_levels = z.shape[0]
+albedo_medium = pyrt.empty_albedo_medium(n_polar)
+diffuse_up_flux = pyrt.empty_diffuse_up_flux(n_user_levels)
+diffuse_down_flux = pyrt.empty_diffuse_down_flux(n_user_levels)
+direct_beam_flux = pyrt.empty_direct_beam_flux(n_user_levels)
+flux_divergence = pyrt.empty_flux_divergence(n_user_levels)
+intensity = pyrt.empty_intensity(n_polar, n_user_levels, n_azimuth)
+mean_intensity = pyrt.empty_mean_intensity(n_user_levels)
+transmissivity_medium = pyrt.empty_transmissivity_medium(n_polar)
+
+# Misc
+user_od_output = np.zeros(n_user_levels)
+temper = np.zeros(n_user_levels)
+h_lyr = np.zeros(n_user_levels)
+
+# 8 SZAs (0--70), 8 EAs (0--70), 19 azimuth angles (0--180), 10 Psurf (same ones as Mike), 21 dust OD (0--2), 11 ice OD (0--1), 6 wavelengths (3 short wavelength and 3 long wavelength)
+# Why azimuth angles instead of phase angles?
+#lut = np.zeros((8, 19, 10, 21, 11, 6)) * np.nan
+
+memmap_filename_lut = os.path.join(mkdtemp(), 'myNewFileLUT.dat')
+memmap_filename_lut0 = os.path.join(mkdtemp(), 'myNewFileLUT0.dat')
+lut = np.memmap(memmap_filename_lut, dtype=float, shape=(8, 8, 19, 10, 21, 11, 6), mode='w+') * np.nan
+lut0 = np.memmap(memmap_filename_lut0, dtype=float, shape=(8, 8, 19, 10, 21, 11, 6), mode='w+') * np.nan
+
+def process_lut(a, sza, b, ea):
+    for c, azimuth in enumerate(np.arange(19) * 10):
+        mu0 = np.cos(np.radians(sza))
+        mu = np.cos(np.radians(ea))
+
+        # The surface part straight up gives nonsense when calling DISORT. I'm keeping it here because this is how I ought to do it,
+        #  but I'm not using it. I would do it... but there's no documentation of what these damn arrays are even supposed to be
+        #  so any attempts to debug would just be guesswork
+        surfaces = [pyrt.Surface(0.01, n_streams, n_polar, n_azimuth, True, False) for i in range(6)]
+        for index, i in enumerate(surfaces):
+            i.make_lambertian()
+            '''i.make_hapkeHG2_roughness(0.8, 0.06, wolff_hapke[index], 0.3, 0.45, 20, mu, mu0, azimuth, 0, np.pi)'''
+
+        for d, surface_pressure in enumerate(np.linspace(0.3, 13.3, 10)):
+            pressure = surface_pressure * 100 * np.exp(-z / 10)
+            colden = np.flip(pyrt.column_density(pressure, temperature, z))
+            rayleigh_co2 = pyrt.rayleigh_co2(colden, iuvs_wavelengths)
+
+            dust_profile = pyrt.conrath(altitude_midpoint, 1, 10, 0.01)
+            ice_profile = np.logical_and(25 <= altitude_midpoint, altitude_midpoint <= 50)
+            ice_profile = ice_profile / np.sum(ice_profile)
+
+            for e, dust_od in enumerate(np.arange(21) / 10):
+                ##############
+                # Dust FSP
+                ##############
+                # Get the dust optical depth at 250 nm
+                dust_z_grad = np.linspace(1, 1.5, num=len(z) - 1)
+                ext = pyrt.extinction_ratio(dust_extinction_cross_sections, dust_particle_sizes, dust_wavelengths, 0.25)
+                ext = pyrt.regrid(ext, dust_particle_sizes, dust_wavelengths, dust_z_grad, iuvs_wavelengths)
+                dust_optical_depth = pyrt.optical_depth(dust_profile, colden, ext, dust_od)
+                dust_single_scattering_albedo = pyrt.regrid(
+                    dust_scattering_cross_sections / dust_extinction_cross_sections,
+                    dust_particle_sizes, dust_wavelengths, dust_z_grad, iuvs_wavelengths)
+                dust_legendre = pyrt.regrid(dust_legendre_coefficients, dust_particle_sizes,
+                                            dust_wavelengths, dust_z_grad, iuvs_wavelengths)
+                dust_column = pyrt.Column(dust_optical_depth, dust_single_scattering_albedo, dust_legendre)
+                for f, ice_od in enumerate(np.arange(11) / 10):
+                    ##############
+                    # Ice FSP
+                    ##############
+                    # Get the ice optical depth at 250 nm
+                    ice_z_grad = np.linspace(5, 5, num=len(z) - 1)
+                    ext = pyrt.extinction_ratio(ice_extinction_cross_sections, ice_particle_sizes, ice_wavelengths, 0.25)
+                    ext = pyrt.regrid(ext, ice_particle_sizes, ice_wavelengths, ice_z_grad, iuvs_wavelengths)
+                    ice_optical_depth = pyrt.optical_depth(ice_profile, colden, ext, ice_od)
+                    ice_single_scattering_albedo = pyrt.regrid(
+                        ice_scattering_cross_sections / ice_extinction_cross_sections,
+                        ice_particle_sizes, ice_wavelengths, ice_z_grad, iuvs_wavelengths)
+                    ice_legendre = pyrt.regrid(ice_legendre_coefficients, ice_particle_sizes,
+                                               ice_wavelengths, ice_z_grad, iuvs_wavelengths)
+                    ice_column = pyrt.Column(ice_optical_depth, ice_single_scattering_albedo, ice_legendre)
+
+                    ##############
+                    # Total atmosphere + DISORT call
+                    ##############
+                    atm = rayleigh_co2 + dust_column + ice_column
+
+                    for g, wavelength in enumerate(iuvs_wavelengths):
+                        rfldir, rfldn, flup, dfdt, uavg, uu, albmed, trnmed = \
+                            disort.disort(True, False, False, False, [False, False, False, False, False],
+                                          False, True, True, False,
+                                          atm.optical_depth[:, g],
+                                          atm.single_scattering_albedo[:, g],
+                                          atm.legendre_coefficients[:, :, g],
+                                          temper, 1, 1, user_od_output,
+                                          mu0, 0, mu, azimuth,
+                                          np.pi, 0, clancy_lambert_albedo[g], 0, 0, 1, 3400000, h_lyr,
+                                          surfaces[g].rhoq, surfaces[g].rhou, surfaces[g].rho_accurate, surfaces[g].bemst, surfaces[g].emust,
+                                          0, '', direct_beam_flux,
+                                          diffuse_down_flux, diffuse_up_flux, flux_divergence,
+                                          mean_intensity, intensity, albedo_medium,
+                                          transmissivity_medium, maxcmu=n_streams, maxulv=n_user_levels, maxmom=159)
+
+                        lut[a, b, c, d, e, f, g] = uu[0, 0, 0]
+    return lut, a, b
+
+
+def make_lut():
+    n_cpus = mp.cpu_count()
+    pool = mp.Pool(n_cpus - 1)  # save one/two just to be safe. Some say it's faster
+    # NOTE: if there are any issues in the argument of apply_async (here,
+    # retrieve_ssa), it'll break out of that and move on to the next iteration.
+
+    for a, sza in enumerate(np.arange(8) * 10):
+        for b, ea in enumerate(np.arange(8) * 10):
+            #process_lut(a, sza, b, ea)
+            pool.apply_async(func=process_lut, args=(a, sza, b, ea), callback=make_array)
+
+    # https://www.machinelearningplus.com/python/parallel-processing-python/
+    pool.close()
+    pool.join()  # I guess this postpones further code execution until the queue is finished
+    np.save(f'/home/kyle/lut.npy', lut0)
+
+
+def make_array(input):
+    # I have no idea why this bit is necessary. I couldn't get it to just save the lut after doing the multiprocessing.
+    arr = input[0]
+    ind0 = input[1]
+    ind1 = input[2]
+    lut0[ind0, ind1] = arr[ind0, ind1]
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    make_lut()
+    print(time.time() - t0)
---
+++ data_files/retrievals/lut_retrieval.py
@@ -0,0 +1,197 @@
+from pathlib import Path
+import multiprocessing as mp
+import os
+from tempfile import mkdtemp
+
+from astropy.io import fits
+from h5py import File
+from netCDF4 import Dataset
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+from scipy.optimize import minimize
+
+import pyrt
+
+# lut is at /mnt/science/data_lake/mars/maven/iuvs/lut.npy
+
+class Ames:
+    def __init__(self):
+        self._base_path = Path('/home/kyle/Downloads')
+
+        self._fixed = Dataset(self._base_path / '05344.fixed.nc')
+        self._average = Dataset(self._base_path / '05344.atmos_average.nc')
+        self._diurnal = Dataset(self._base_path / '05344.atmos_diurn.nc')
+
+        self.latitude = self._fixed.variables['lat'][:]
+        self.longitude = self._fixed.variables['lon'][:]
+
+        self.dust_vprof = self._average.variables['dustref']  # shape: (year, pressure level, lat, lon)
+        # self.ice_vprof = self._average.variables['cldref']  # shape: (year, pressure level, lat, lon)
+
+        # self.pressure_levels = self._average.variables['pstd']  # shape: (pressure level,). Index 0 is TOA
+        self.surface_pressure = self._average.variables['ps']  # shape: (season, time of day, lat, lon)
+        self.surface_temperature = self._average.variables['ts']  # shape: (season, time of day, lat, lon)
+        self.temperature = self._average.variables['temp'][:]  # shape: (season, time of day, pressure level, lat, lon)
+        self.season = self._make_season()
+        self.local_time = self._diurnal.variables['time_of_day_24'][:]
+
+        self.ak = self._fixed.variables['ak'][:]
+        self.bk = self._fixed.variables['bk'][:]
+
+    def _make_season(self):
+        time = self._average.variables['time'][:]
+        delta = time[1] - time[0]
+        time -= delta / 2
+        time -= time[0]
+        return time
+
+    def get_seasonal_index(self, sol):
+        return np.argmin(np.abs(self.season - sol))
+
+    def get_latitude_index(self, lat: float):
+        return np.argmin(np.abs(self.latitude - lat))
+
+    def get_longitude_index(self, lon: float):
+        return np.argmin(np.abs(self.longitude - lon))
+
+    def get_local_time_index(self, lon: float, lt):
+        return np.argmin(np.abs(self.local_time - (lt - (lon / 360 * 24)) % 24))
+
+    def make_pressure(self):
+        return np.multiply.outer(self.surface_pressure, self.bk) + self.ak  # shape: (season, lat, lon, altitude)
+
+    def get_pixel_pressure(self, lat, lon, sol):
+        # Make the pressure and temperature vertical profiles from the surface
+        lat_idx = self.get_latitude_index(lat)
+        lon_idx = self.get_longitude_index(lon)
+        season_idx = self.get_seasonal_index(sol)
+
+        surface_pressure = self.surface_pressure[season_idx, lat_idx, lon_idx]
+        return np.multiply.outer(surface_pressure, self.bk) + self.ak  # This is a huge performance improvement by not computing all the atmospheric pressures
+
+    def get_pixel_temperature(self, lat, lon, sol):
+        lat_idx = self.get_latitude_index(lat)
+        lon_idx = self.get_longitude_index(lon)
+        season_idx = self.get_seasonal_index(sol)
+
+        surface_temperature = self.surface_temperature[season_idx, lat_idx, lon_idx]
+
+        # Get where the surface index would be
+        surface_idx = np.argmin(~self.temperature.mask)
+
+        return np.insert(self.temperature[season_idx, :, lat_idx, lon_idx], surface_idx, surface_temperature)
+
+    def get_pixel_boundary_altitude(self, lat, lon, sol):
+        pressure = self.get_pixel_pressure(lat, lon, sol)
+        return -np.log(pressure / pressure[-1]) * 10
+
+    def get_dust_profile(self, lat, lon, sol):
+        lat_idx = self.get_latitude_index(lat)
+        lon_idx = self.get_longitude_index(lon)
+        season_idx = self.get_seasonal_index(sol)
+
+        return self._average.variables['dustref'][season_idx, :, lat_idx, lon_idx]
+
+    def get_season_surface_pressure(self, sol):
+        season_idx = self.get_seasonal_index(sol)
+        return self.surface_temperature[season_idx]
+
+
+orbit = 3453
+# Read in external files
+lut = np.load('/home/kyle/lut.npy')
+radiance_files = sorted(Path('/home/kyle/iuvs/radiance03453').glob(f'orbit*{orbit}*'))
+fits_files = sorted(Path('/media/kyle/iuvs/production/orbit03400').glob('*apoapse*orbit03453*muv*.gz'))
+hduls = [fits.open(f) for f in fits_files]
+apsis_file = File('/media/kyle/iuvs/apsis.hdf5')
+
+# Make the sol info
+sol = apsis_file['apoapse/sol'][3453]
+
+# Make the interpolation
+sza_grid = np.arange(8) * 10
+ea_grid = np.arange(8) * 10
+azimuth_grid = np.arange(19) * 10
+pressure_grid = np.linspace(0.3, 13.3, 10) * 100
+dust_grid = np.arange(21) / 10
+ice_grid = np.arange(11) / 10
+wavelength_grid = np.array([0.210, 0.215, 0.220, 0.272, 0.278, 0.284])
+
+interp = RegularGridInterpolator((sza_grid, ea_grid, azimuth_grid, pressure_grid, dust_grid, ice_grid, wavelength_grid), lut)
+
+# Get pressure info
+gcm = Ames()
+season_surface_pressure = gcm.get_season_surface_pressure(sol)
+sfc_interp = RegularGridInterpolator((gcm.latitude, gcm.longitude), season_surface_pressure)
+
+
+for counter, radiance in enumerate(radiance_files):
+    rad = np.load(radiance)
+    szas = hduls[counter]['pixelgeometry'].data['pixel_solar_zenith_angle']
+    eas = hduls[counter]['pixelgeometry'].data['pixel_emission_angle']
+    pas = hduls[counter]['pixelgeometry'].data['pixel_phase_angle']
+    alts = hduls[counter]['pixelgeometry'].data['pixel_corner_mrh_alt'][:, :, 4]
+    azimuths = pyrt.azimuth(szas, eas, pas)
+    latitudes = hduls[counter]['pixelgeometry'].data['pixel_corner_lat'][:, :, 4]
+    longitudes = hduls[counter]['pixelgeometry'].data['pixel_corner_lon'][:, :, 4]
+
+    if rad.shape[-1] == 20:
+        wavelength_indices = [1, 2, 3, -8, -7, -6]
+    elif rad.shape[-1] == 19:
+        wavelength_indices = [1, 2, 3, -7, -6, -5]
+    elif rad.shape[-1] == 15:
+        wavelength_indices = [1, 2, 3, -3, -2, -1]
+
+    memmap_filename_answer = os.path.join(mkdtemp(), 'myNewFileAnswer.dat')
+    answer = np.memmap(memmap_filename_answer, dtype=float, shape=latitudes.shape + (9,), mode='w+') * np.nan # 9 is for dust, ice, error, simulated I/F at 6 wavelengths
+    answer0 = np.zeros(latitudes.shape + (9,)) * np.nan
+
+    def fit_dust_and_ice(radiance, sza, ea, aza, psurf) -> float:
+        def find_best_fit(guess: np.ndarray):
+            dust_guess = guess[0]
+            ice_guess = guess[1]
+            simulated_spectrum = np.zeros(wavelength_grid.shape) * np.nan
+            for c, wav in enumerate(wavelength_grid):
+                input = np.array([sza, ea, aza, psurf, dust_guess, ice_guess, wav])
+                simulated_spectrum[c] = interp(input)
+            return np.sum((simulated_spectrum - radiance[wavelength_indices])**2)
+
+        return minimize(find_best_fit, np.array([0.7, 0.2]), method='Nelder-Mead', tol=1e-2, bounds=((0, 2), (0, 1)), options={'adaptive': True}).x
+
+    def make_array(input):
+        # I have no idea why this bit is necessary. I couldn't get it to just save the lut after doing the multiprocessing.
+        ind0 = input[0]
+        ind1 = input[1]
+        arr = input[2]
+
+        answer0[ind0, ind1] = arr
+
+    def do_retrieval(integration, spatial_bin):
+        print(integration, spatial_bin)
+        if alts[integration, spatial_bin] != 0 or szas[integration, spatial_bin] > 70 or eas[integration, spatial_bin] > 70:
+            return integration, spatial_bin, np.zeros(9,) * np.nan
+        sfc_pressure = sfc_interp(np.array([latitudes[integration, spatial_bin], longitudes[integration, spatial_bin]]))[0]
+        answer[integration, spatial_bin, :2] = fit_dust_and_ice(rad[integration, spatial_bin], szas[integration, spatial_bin], eas[integration, spatial_bin], azimuths[integration, spatial_bin], sfc_pressure)
+
+        # Get the spectrum of the best fit answer
+        sim_spec = np.zeros(6,)
+        for c, wav in enumerate(wavelength_grid):
+            sim_spec[c] = interp(np.array([szas[integration, spatial_bin], eas[integration, spatial_bin], azimuths[integration, spatial_bin], sfc_pressure, answer[integration, spatial_bin, 0], answer[integration, spatial_bin, 1], wav]))
+        error = np.sum((rad[integration, spatial_bin, wavelength_indices] - sim_spec)**2 / sim_spec)
+        #error = np.sum(np.abs((rad[integration, spatial_bin, wavelength_indices] - sim_spec)**2/rad[integration, spatial_bin, wavelength_indices]))
+
+        # Add the spectrum to the array
+        answer[integration, spatial_bin, 2] = error
+        answer[integration, spatial_bin, 3:] = sim_spec
+        return integration, spatial_bin, answer[integration, spatial_bin]
+
+    n_cpus = mp.cpu_count()
+    pool = mp.Pool(n_cpus - 1)
+    for integration in range(rad.shape[0]):
+        for spatial_bin in range(rad.shape[1]):
+            pool.apply_async(func=do_retrieval, args=(integration, spatial_bin), callback=make_array)
+
+    # https://www.machinelearningplus.com/python/parallel-processing-python/
+    pool.close()
+    pool.join()
+    np.save(f'/home/kyle/iuvs/radiance03453/{str(counter).zfill(2)}new.npy', answer0)
