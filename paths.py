6d9370b16c3787c35b6bf374eec4cc20be8b69e8
Kyle <kyle.d.connour@gmail.com>
2024-07-24 13:04:01 -0600

    Add paths file to describe locations on this system

---
+++ paths.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+apsis_file_path = Path('/mnt/science/mars/missions/maven/apsis.hdf5')
+"""This is the location of where the apsis file lives on this computer."""
+
+spice_kernel_location = Path('/mnt/science/mars/missions/maven/spice_kernels')
+"""This is the location of where the SPICE kernels live on this computer."""
