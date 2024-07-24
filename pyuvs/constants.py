00cae63a81382ad08e1346932cad737b05bf0988
Kyle <kyle.d.connour@gmail.com>
2024-07-24 13:04:20 -0600

    Add pyuvs package with some basic constants

---
+++ pyuvs/constants.py
@@ -0,0 +1,7 @@
+"""This module contains constants related to MAVEN and IUVS."""
+
+slit_start_pixel = 80
+"""The is the starting spatial pixel number."""
+
+slit_end_pixel = 929
+"""The is the ending spatial pixel number."""
