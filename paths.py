0a3d04229f6362a32049121c449e0e1a4cc478b4
Kyle <kyle.d.connour@gmail.com>
2024-07-26 15:24:52 -0600

    Update paths to include more locations

--- paths.py
+++ paths.py
@@ -6,3 +6,9 @@ apsis_file_path = Path('/mnt/science/mars/missions/maven/apsis.hdf5')
 
 spice_kernel_location = Path('/mnt/science/mars/missions/maven/spice_kernels')
 """This is the location of where the SPICE kernels live on this computer."""
+
+orbit_file_path = Path('/media/kyle/iuvs/data')
+"""This is the location of where data files for each orbit live on this computer."""
+
+iuvs_fits_files_location = Path('/mnt/science/mars/missions/maven/instruments/iuvs/production')
+"""This is the location of where IUVS .fits files live on this computer."""
