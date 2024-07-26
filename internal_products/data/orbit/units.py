7ff8012f8f4f13ca93943cca3bd5510f69937564
Kyle <kyle.d.connour@gmail.com>
2024-07-26 15:26:22 -0600

    Add unit definitions

---
+++ internal_products/data/orbit/units.py
@@ -0,0 +1,21 @@
+"""This module provides unit definitions for use in data file processing.
+"""
+
+altitude: str = 'km'
+angle: str = 'Degrees'
+bins: str = 'Bins'
+bin_number: str = 'Bin number'
+brightness: str = 'kR'
+data_number: str = 'Data number'
+ephemeris_time: str = 'Seconds since J2000'
+integration_time: str = 'Seconds'
+latitude: str = 'Degrees [N]'
+local_time: str = 'Hours'
+longitude: str = 'Degrees [E]'
+mars_year: str = 'Years'
+sol: str = 'Day of the year'
+solar_longitude: str = 'Degrees'
+temperature: str = 'Degrees [C]'
+unit_vector: str = 'Unit vector'
+velocity: str = 'km/s'
+voltage: str = 'Volts'
